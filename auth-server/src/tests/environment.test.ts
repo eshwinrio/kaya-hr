edae519d3b62b85997b7db8d91fa403b688a70bf
Eshwin Rio <riopinto487@gmail.com>
2024-02-05 04:43:42 -0500

    Prerequisite setup done

---
+++ auth-server/src/tests/environment.test.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+
+describe('HTTP configurations', () => {
+  it('HTTP port', (done) => {
+    const value = process.env['HTTP_PORT'];
+    expect(value).not.to.be.undefined;
+    const parsed = parseInt(value!);
+    expect(parsed).not.to.be.NaN;
+    expect(parsed).to.be.above(0).below(9999);
+    done();
+  });
+});
