35f85db9fe76d4f393c35ecb36b70b2d15b25aa4
Eshwin Rio <riopinto487@gmail.com>
2024-02-05 14:17:28 -0500

    Configured prisma with 4 models

--- auth-server/src/bin/www/index.ts
+++ auth-server/src/bin/www/index.ts
@@ -1,15 +1,54 @@
-import { createServer } from "http";
+import { Server as HttpServer, createServer } from "http";
+import { Server as HttpsServer } from "https";
 import app from "../../app.js";
 import { logSystem } from "../../lib/logger.js";
+import prisma from "../../lib/prisma.js";
+import { Http } from "../../config/environment.js";
 
-const server = createServer(app);
+let server: HttpServer | HttpsServer | null = null;
 
-server.listen(process.env.HTTP_PORT);
+async function init() {
+  logSystem.debug(`Server startup | PID:${process.pid}`);
+  try {
+    await prisma.$connect();
+    server = createServer(app);
+    server.addListener("listening", () => logSystem.info(`Server started on port ${Http.port}`));
+    server.listen(Http.port);
 
-server.addListener("error", (e) => {
-  logSystem.error(e);
+    process.on('beforeExit', async (code) => {
+      try {
+        logSystem.debug(
+          `SIG:${code} - Releasing resources/Performing cleanup...`
+        );
+        await prisma.$disconnect();
+        server?.close();
+      } catch (error) {
+        logSystem.error('Cleanup failed', error);
+      }
+    });
+  } catch (e) {
+    logSystem.error(e);
+    process.exit(1);
+  } finally {
+    logSystem.debug(`Finalized server startup | PID:${process.pid}`);
+  }
+}
+
+process.on("unhandledRejection", (error) => {
+  logSystem.error(error);
+  process.exit(1);
 });
 
-server.addListener("listening", () => {
-  logSystem.info(`Server started on port ${process.env.HTTP_PORT}`);
+process.on("uncaughtException", (error) => {
+  logSystem.error(error);
+  process.exit(1);
 });
+
+process.on("SIGTERM", (status) => {
+  if (server) {
+    server.close();
+  }
+  logSystem.debug(`[SIG:${status}] Exiting process...`);
+});
+
+init();
--- auth-server/src/tests/environment.test.ts
+++ auth-server/src/tests/environment.test.ts
@@ -1,5 +1,13 @@
 import { expect } from "chai";
 
+describe('Database configurations', () => {
+  it('Database URL', (done) => {
+    const value = process.env['DATABASE_URL'];
+    expect(value).not.to.be.undefined;
+    done();
+  });
+});
+
 describe('HTTP configurations', () => {
   it('HTTP port', (done) => {
     const value = process.env['HTTP_PORT'];
