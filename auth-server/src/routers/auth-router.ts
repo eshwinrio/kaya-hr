7048296b8044e5b836a06ba862f105139918a3a9
Eshwin Rio <riopinto487@gmail.com>
2024-02-09 10:08:29 -0500

    email-paswd token generation with basic validation done

--- auth-server/src/routers/auth-router.ts
+++ auth-server/src/routers/auth-router.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import httpErrors, { HttpError } from "http-errors";
-import prisma from "../lib/prisma";
+import { compare } from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+import prisma from "../lib/prisma.js";
+import requireBody from "../middlewares/require-body.js";
+import { JWT } from "../config/environment.js";
 
 const authRouter = Router();
 
-
+type TokenGenReqBody = Record<"username" | "password", string>;
 authRouter.post(
   "/auth/token",
-  // TODO: implement requireBody middleware
-  async (req, res) => {
+  requireBody<TokenGenReqBody>("username", "password"),
+  async (req, res, next) => {
     try {
       const { username, password } = req.body;
 
@@ -22,9 +26,17 @@ authRouter.post(
         throw httpErrors.NotFound("User not found");
       }
 
-      return res.json("token"); // TODO: implement token generation
-    } catch (error) {
+      if ((await compare(password, user.password)) === false) {
+        throw httpErrors.Unauthorized("Invalid credentials");
+      }
 
+      const token = jsonwebtoken.sign({ id: user.id }, JWT.accessSecret, {
+        expiresIn: JWT.accessValidity
+      });
+
+      return res.json({ accessToken: token });
+    } catch (error) {
+      next(error);
     }
   }
 )
