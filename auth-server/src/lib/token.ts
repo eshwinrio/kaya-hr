334481848343c404676d3312f8cd2939a4c3cfc3
Eshwin Rio <riopinto487@gmail.com>
2024-02-18 15:14:07 -0500

    Updated token generator to include application data

--- auth-server/src/lib/token.ts
+++ auth-server/src/lib/token.ts
@@ -1,14 +1,23 @@
-import { Roles, Users } from "@prisma/client";
+import { Applications, Roles, Users } from "@prisma/client";
 import jsonwebtoken, { JwtPayload } from "jsonwebtoken";
 import { JWT } from "../config/environment.js";
 
-export type AccessTokenPayload = JwtPayload & Partial<Users> & { roles: Roles[] };
+export type AccessTokenPayload =
+  JwtPayload
+  & Pick<Users, 'id' | 'email' | 'firstName' | 'middleName' | 'lastName'>
+  & Record<'application', Applications['secret']>
+  & Record<'roles', Roles[]>;
 
 export function generateAccessToken(
   { id, email, firstName, middleName, lastName }: Users,
+  { secret }: Applications,
   roles: Roles[]
 ): string {
-  const payload: AccessTokenPayload = { id, email, firstName, middleName, lastName, roles };
+  const payload: AccessTokenPayload = {
+    id, email, firstName, middleName, lastName,
+    application: secret,
+    roles
+  };
   return jsonwebtoken.sign(payload, JWT.accessSecret, {
     expiresIn: JWT.accessValidity
   });
---
+++ auth-server/src/middlewares/require-application.ts
@@ -0,0 +1,43 @@
+import { RequestHandler } from 'express';
+import httpErrors from 'http-errors';
+import qs from 'qs';
+import validator from 'validator';
+import jsonwebtoken from 'jsonwebtoken';
+import prisma from '../lib/prisma.js';
+import { Applications } from '@prisma/client';
+
+type Locals = Record<'application', Applications>;
+type ApplicationEnforcer = RequestHandler<any, any, any, qs.ParsedQs, Locals>;
+
+export default function (): ApplicationEnforcer {
+  return async (request, response, next) => {
+    try {
+      const applicationSecret = request.get('X-Application');
+      if (!applicationSecret) {
+        throw httpErrors.Unauthorized('Missing application secret');
+      }
+
+      if (!validator.isUUID(applicationSecret)) {
+        throw httpErrors.Unauthorized('Invalid application secret');
+      }
+
+      const application = await prisma.applications.findFirst({
+        where: {
+          secret: applicationSecret
+        }
+      });
+      if (!application) {
+        throw httpErrors.NotFound('Application not found');
+      }
+      
+      response.locals.application = application;
+      next();
+    } catch (error) {
+      if (error instanceof jsonwebtoken.TokenExpiredError) {
+        response.clearCookie('access_token');
+        return next(httpErrors.Unauthorized('Access token expired'));
+      }
+      next(error);
+    }
+  };
+}
