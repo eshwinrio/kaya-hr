613682b3c4299799d6e8ee25cf4ecc764c29f466
Kalpesh406 <prajapatikalpesh406@gmail.com>
2024-02-19 15:44:01 -0500

    All the changes are done.

--- hr-panel/src/lib/fetch-requests.ts
+++ hr-panel/src/lib/fetch-requests.ts
@@ -1,17 +1,26 @@
 export const verifyIdentity = (requestInit?: RequestInit) => fetch(
   `${process.env['REACT_APP_AUTH_API_DOMAIN']}/auth/verify`,
-  { credentials: 'include', ...requestInit }
+  {
+    method: 'GET',
+    headers: {
+      'X-Application': process.env['REACT_APP_APPLICATION_SECRET']!,
+    },
+    credentials: 'include',
+    ...requestInit
+  }
 );
 
-export const fetchAccessToken = (username: string, password: string) => fetch(
+export const fetchAccessToken = (username: string, password: string, requestInit?: RequestInit) => fetch(
   `${process.env['REACT_APP_AUTH_API_DOMAIN']}/auth/token`,
   {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'X-Application': process.env['REACT_APP_APPLICATION_SECRET']!,
     },
     credentials: 'include',
     body: JSON.stringify({ username, password }),
+    ...requestInit
   }
 );
 
