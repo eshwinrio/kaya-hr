617a098cb7d078e10d9b9ba55a0e26424f2036d0
Eshwin Rio <riopinto487@gmail.com>
2024-02-26 14:34:08 -0500

    `gql-codegen` configured

--- hr-panel/codegen.ts
+++ hr-panel/codegen.ts
@@ -1,10 +1,13 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: CodegenConfig = {
   schema: process.env['REACT_APP_RESOURCE_API_DOMAIN']! + process.env['REACT_APP_RESOURCE_API_GRAPHQL_ENDPOINT'],
-  documents: ['src/**/*.{ts,tsx}'],
+  documents: 'src/lib/gql-queries.ts',
   generates: {
-    './src/__generated__/': {
+    './src/lib/gql-codegen/': {
       preset: 'client',
       plugins: [],
       presetConfig: {
@@ -13,6 +16,7 @@ const config: CodegenConfig = {
     }
   },
   ignoreNoDocuments: true,
+  verbose: true,
 };
 
 export default config;
