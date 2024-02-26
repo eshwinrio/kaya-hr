c64b3577b852d7893fb83e4de7feaf0d893beea1
Eshwin Rio <riopinto487@gmail.com>
2024-02-26 15:36:26 -0500

    configured cors

--- hr-resource/src/app.ts
+++ hr-resource/src/app.ts
@@ -1,10 +1,28 @@
 import express from "express";
-import { httpLogStream } from "./lib/logger.js";
-import apolloServer from "./lib/apollo.js";
+import cors, { CorsOptions } from "cors";
 import { expressMiddleware } from "@apollo/server/express4";
+import { httpLogStream } from "./lib/logger.js";
+import apolloServer, { ApolloServerContext } from "./lib/apollo.js";
+import { Cors } from "./config/environment.js";
 
 const app = express();
 
+
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      return callback(null, true);
+    }
+    return Cors.origins.includes(origin)
+      ? callback(null, true)
+      : callback(new Error("Not allowed by CORS"));
+  },
+  methods: Cors.methods,
+  credentials: Cors.allowCredentials,
+  maxAge: Cors.maxAge,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(httpLogStream);
 
--- hr-resource/src/config/environment.ts
+++ hr-resource/src/config/environment.ts
@@ -1,3 +1,10 @@
+export namespace Cors {
+  export const origins = process.env['CORS_ORIGINS']!.split(',').map((o) => o.trim());
+  export const methods = process.env['CORS_METHODS']!.split(',').map((m) => m.trim());
+  export const allowCredentials = process.env['CORS_ALLOW_CREDENTIALS'] === 'true';
+  export const maxAge = parseInt(process.env['CORS_MAX_AGE']!);
+}
+
 export namespace Http {
   export const port = parseInt(process.env['HTTP_PORT']!);
 }
