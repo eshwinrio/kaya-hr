0d95f4705f2150a861acafdea23b0bf7b10274cf
Kalpesh406 <prajapatikalpesh406@gmail.com>
2024-03-25 16:21:02 -0400

    improvement done

--- kaya-landing/src/pages/LandingPage.jsx
+++ kaya-landing/src/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import { Typography, Container } from '@mui/material';
 import banner from '../assets/banner.svg';
 import '../css/style.css';
 
+
 export default function LandingPage() {
   return (
     <div id='landing-page'>
@@ -17,6 +18,23 @@ export default function LandingPage() {
         </div>
         <img src={banner} alt="banner" width={450} id='banner-img' />
       </Container>
+      <Typography variant='h3' align='center' className='landing-header' mb={1} mt={20}>Collaborations</Typography>
+      <div class="flex-container">
+        <div class="grid-item">
+          <img src="https://southstreetburger.com/wp-content/uploads/2021/07/ssb-logo-white-1.png" alt="" srcset="" />
+        </div>
+        <div class="grid-item">
+          <img src="https://choiceindianrestaurant.ca/wp-content/uploads/2022/09/IMG-20220914-WA0000-removebg-preview.png" alt="" srcset="" />
+        </div>
+        <div class="grid-item">
+          <img src="https://www.thegrandmehfil.com/wp-content/uploads/2021/02/Final-logo-01.png" alt="" srcset="" />
+        </div>
+        <div class="grid-item">
+          <img src="https://mma.prnewswire.com/media/1688892/Clutch_Logo_Red__1.jpg?p=facebook" alt="" srcset="" />
+        </div>
+
+      </div><hr></hr>
+      <Typography align='center'>&copy; Made with love 	&hearts;</Typography>
     </div>
   );
 }
